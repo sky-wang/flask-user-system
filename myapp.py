bd9c850a34ccf8fee93f17243fb818e96ef9fb68
whu_liuchang <nfcj000@qq.com>
2013-03-05 14:57:21 +0800

    完成整体功能 to-do: 编辑信息 发邮件验证

--- myapp.py
+++ myapp.py
@@ -1,6 +1,6 @@
-#encoding=utf8
+# -*- coding: utf-8 -*-
 from flask import Flask
-from flask import  render_template , request , redirect , url_for ,flash
+from flask import  render_template , request , redirect , url_for ,flash , session
 from flask import g
 from flask.ext import sqlalchemy
 from flask import Config
@@ -14,8 +14,8 @@ app.config['SECRET_KEY'] = SECRET_KEY
 db = sqlalchemy.SQLAlchemy(app)
 
 class user(db.Model):
-    UID = db.Column(db.Integer , primary_key=True,autoincrement=True) #id
-    Username = db.Column(db.String , nullable=False,unique=True)
+    UID = db.Column(db.Integer , primary_key=True,autoincrement=True,unique=True) #id
+    Username = db.Column(db.String , nullable=False,)
     Password = db.Column(db.String , nullable=False)
     Email = db.Column(db.String ,nullable = False)
     School = db.Column(db.String , nullable = True)
@@ -23,7 +23,7 @@ class user(db.Model):
     Blog = db.Column(db.String)
     Introduction = db.Column(db.String)
 
-    def __init__(self , username , password , email , school = 'none' , age = 0 , blog='http://baidu.com',introdution='please introduce youself!'):
+    def __init__(self , username , password , email , school = 'none' , age = 0 , blog='http://baidu.com',introdution='i am too lazy~~'):
         self.Username = username
         self.Password = password
         self.Email = email
@@ -35,8 +35,6 @@ class user(db.Model):
         return '<User %s>'%self.Username
 
 def vali(newone):
-    if user.query.filter_by(Username=newone.Username).count() > 0:
-        return 0
     if len(newone.Password) < 6:
         return 0
     return 1
@@ -44,35 +42,75 @@ def vali(newone):
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
 @app.route('/reg',methods=['GET','POST'])
 def reg():
     if request.method == 'GET':
+        print "get"
         return render_template('reg.html')
     else:
+        print "post"
         username = request.form['username']
         password = request.form['password']
+        repassword = request.form['repassword']
         email = request.form['email']
-        age = request.form['age']
-        school = request.form['school']
-        blog = request.form['blog']
-        intro = request.form['intro']
-        newuser = user(username , password , email , age , school , blog , intro)
+
+        print "< %s %s %s>" % (username , password ,email)
+        if password != repassword:
+            flash("passwords doesn't match!!")
+            return render_template('reg.html')
+
+        newuser = user(username , password , email )
+        print newuser
         if vali(newuser):
             db.session.add(newuser)
             db.session.commit()
-            redirect(url_for(userinfo))
+            flash("Registration is successful!")
+            return redirect(url_for('info'))
         else :
-            render_template('tip.html',msg='注册信息不合法！')
-@app.route('/login')
-def login():
-    return render_template('login.html')
+            flash("Registration is not successful!")
+            return redirect(url_for('login'))
 
-@app.route('/loginout')
-def loginout():
-    pass
+@app.route('/login' , methods=['GET','POST'])
+def login():
+    if 'username' in session:
+        flash("Already logged in, please exit!")
+        redirect(url_for('info'))
+    else :
+        if request.method == 'GET':
+            return render_template('login.html')
+        else :
+            email = request.form['email']
+            password = request.form['password']
+            User = user.query.filter_by(Email = email , Password = password).first()
+            if User is None:
+                flash("email or password is wrong!!")
+                return render_template('login.html')
+            else:
+                session['username'] = User.Username
+                session['email'] = User.Email
+                flash("login successful!")
+                return render_template('info.html',user = User)
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.clear()
+        return render_template('sucexit.html')
+    else:
+        return render_template('nologin.html')
+
 @app.route('/info')
-def userinfo():
-    pass
+def info():
+    if 'username' in session:
+        User = user.query.filter_by(Email = session['email']).first()
+        return render_template('info.html',user =User)
+    else :
+        flash("Please login system first !!")
+        return redirect(url_for('login'))
+
+
 @app.route('/edit')
 def editinfo():
     pass
