7818f3e9316a86739af3b92275daaaf76fd28652
whu_liuchang <nfcj000@qq.com>
2013-03-06 14:57:08 +0800

    sae 初始版本

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 __author__ = 'liuchang'
+import sae.const
+DB = sae.const.MYSQL_DB      # 数据库名
+USER = sae.const.MYSQL_USER    # 用户名
+PASS = sae.const.MYSQL_PASS    # 密码
+HOST = sae.const.MYSQL_HOST    # 主库域名（可读写）
+PORT = sae.const.MYSQL_PORT    # 端口，类型为<type 'str'>，请根据框架要求自行转换为int
 
-DATABASE = 'sqlite:///db/database.db'
+DATABASE = 'mysql://%s:%s@%s:%d/%s' %(USER,PASS,HOST,int(PORT),DB)
 SECRET_KEY  = 'liuchang0812@gmail.com'
 SMTP_HOST = 'smtp.qq.com'
 SMTP_PORT = '587'
--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'liuchang'
-
+import sae.mail
 from config import  SMTP_HOST , SMTP_PASS , SMTP_PORT , SMTP_USER
 from smtplib import SMTP
 import mimetypes
@@ -9,16 +9,6 @@ from email.mime.text import MIMEText
 
 
 def sendmail(to , subject , body):
-    msg = MIMEText(body)
-    msg['From'] = SMTP_USER
-    msg['To'] = to
-    msg['Subject'] = subject
-    smtp = SMTP()
-    smtp.connect(SMTP_HOST)
-    smtp.login(SMTP_USER,SMTP_PASS)
-    smtp.sendmail(SMTP_USER,to,msg.as_string())
-    smtp.quit()
-    print 'send succsessful!'
-
+    sae.mail.send_mail(to,subject,body,(SMTP_HOST,SMTP_PORT,SMTP_USER,SMTP_PASS,True)) ;
 
 #sendmail('liuchang0812@gmail.com','hello lc ,can you teach me Cplusplus','hello , this is a email send by python')
